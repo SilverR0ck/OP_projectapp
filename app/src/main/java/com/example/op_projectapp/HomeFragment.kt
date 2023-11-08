870dc548d0c774bc223870bcb7c4a970db1896f0
Lee-EunSeok <lanmecan@naver.com>
2023-11-08 15:15:32 +0900

    Initial commit

---
+++ app/src/main/java/com/example/op_projectapp/HomeFragment.kt
@@ -0,0 +1,35 @@
+package com.example.op_projectapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.op_projectapp.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    var binding: FragmentHomeBinding?= null
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentHomeBinding.inflate(inflater)
+        // Inflate the layout for this fragment
+        return binding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding?.btnAddwork?.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment_to_addworkFragment)
+        }
+    }
+
+
+}
